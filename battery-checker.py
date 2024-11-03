6f04a29238b52a517da55c9f961770edcb448b2c
pramod-lak <lakshanvvr@gmail.com>
2024-11-03 17:01:01 +0530

    Modified using psutil
    Modified by adding Battery Percentage and Charging Status

--- battery-checker.py
+++ battery-checker.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+import psutil
 
 # Name of the log file
 log_file = "battery_log.txt"
@@ -7,11 +8,11 @@ log_file = "battery_log.txt"
 # Open the file in append mode (creates the file if it doesn't exist)
 with open(log_file, "a") as file:
     # Write the table headings
-    file.write("Date\t\tTime\t\tElapsed Time\n")
+    file.write("Date | Time | Elapsed Time | Battery Percentage | Charging Status\n\n")
     
     # Print the initial message to the terminal
     print("Battery checker running...")
-    
+
     # Get the start time
     start_time = time.time()
     
@@ -31,8 +32,17 @@ with open(log_file, "a") as file:
         # Format elapsed time as HH:MM:SS
         elapsed_time_str = f"{hours:02}:{minutes:02}:{seconds:02}"
 
+        # Get battery information
+        battery = psutil.sensors_battery()
+        if battery:
+            battery_percentage = f"{battery.percent}%"
+            charging_status = "Charging" if battery.power_plugged else "Not Charging"
+        else:
+            battery_percentage = "N/A"
+            charging_status = "N/A"
+
         # Prepare log message
-        log_message = f"{date_str}\t{time_str}\t{elapsed_time_str}"
+        log_message = f"{date_str}\t{time_str}\t{elapsed_time_str}\t{battery_percentage}\t{charging_status}"
 
         # Write to file
         file.write(log_message + "\n")
